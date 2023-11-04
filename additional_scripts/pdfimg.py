61efeadf17f1f3630915ac2cdae44b913684ce00
kushal-10 <kushalkoshti11@gmail.com>
2023-11-04 14:30:28 +0100

    add plots

--- additional_scripts/pdfimg.py
+++ additional_scripts/pdfimg.py
@@ -1,8 +1,21 @@
+# CONVERT PDF TO JPG
+
 from pdf2image import convert_from_path
  
- 
-paths = ["results_eval/results_eval/results_eval/episode-level/plots/overview_in01.pdf"]
+#ALL, PV/SH,  REF, IMAGE, TABOO, WORDLE, 
+paths = ["results_eval/results_eval/results_eval/episode-level/plots/overview_in01.pdf",
+         "results_eval/results_eval/privateshared/results_eval/episode-level/plots/privateshared_overview-lines.pdf",
+         "results_eval/results_eval/referencegame/results_eval/episode-level/plots/referencegame_overview-lines.pdf",
+         "results_eval/results_eval/imagegame/results_eval/episode-level/plots/imagegame_overview-lines.pdf",
+         "results_eval/results_eval/taboo/results_eval/episode-level/plots/taboo_overview-lines.pdf",
+         "results_eval/results_eval/wordle/results_eval/episode-level/plots/wordle_overview-lines.pdf",
+         "results_eval/results_eval/wordle_withclue/results_eval/episode-level/plots/wordle_withclue_overview-lines.pdf",
+         "results_eval/results_eval/wordle_withcritic/results_eval/episode-level/plots/wordle_withcritic_overview-lines.pdf"
+         ]
+
+img_paths = [path.replace('.pdf', '.jpg') for path in paths]
 
 # Store Pdf with convert_from_path function
-images = convert_from_path(paths[0])
-images[0].save('results_eval/results_eval/results_eval/episode-level/plots/overview_in01.jpg', 'JPEG')
+for i in range(len(paths)):
+    images = convert_from_path(paths[i])
+    images[0].save(img_paths[i], 'JPEG')
